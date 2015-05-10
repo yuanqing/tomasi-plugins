f58f98076cc6c6ba6fe06f5c437c2a5a5414b1eb
yuanqing <hello@yuanqing.sg>
2015-05-10 21:05:10 +0800

    Use `opts`

--- paginate/index.js
+++ paginate/index.js
@@ -2,27 +2,27 @@
 
 var rampage = require('rampage');
 
-var paginate = function(numPerPage, preProcess, postProcess) {
+var paginate = function(numPerPage, opts) {
+  opts = opts || {};
+  opts.preProcess = opts.preProcess || function(files) {
+    return {
+      $content: files
+    };
+  };
+  opts.postProcess = opts.postProcess || function(currPage, prevPage,
+      nextPage, pageNum, totalPages) {
+    currPage.$page = pageNum + 1;
+    currPage.$totalPages = totalPages;
+    currPage.$previous = prevPage || null;
+    currPage.$next = nextPage || null;
+    return currPage;
+  };
   if (numPerPage == null) {
     numPerPage = 1;
-    preProcess = function(files) {
+    opts.preProcess = function(files) {
       return files[0];
     };
   }
-  var opts = {
-    preProcess: preProcess || function(files) {
-      return {
-        $content: files
-      };
-    },
-    postProcess: postProcess || function(currPage, prevPage, nextPage, pageNum, totalPages) {
-      currPage.$page = pageNum + 1;
-      currPage.$totalPages = totalPages;
-      currPage.$previous = prevPage || null;
-      currPage.$next = nextPage || null;
-      return currPage;
-    }
-  };
   return function(cb, files) {
     cb(null, rampage(files, numPerPage, opts));
   };
